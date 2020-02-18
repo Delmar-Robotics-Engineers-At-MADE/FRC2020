f4e6a222299066dd8f7b46aa0ebd381fdeb4fb7c
Matt <slylock2@mac.com>
2020-02-18 13:31:45 -0600

    NavX rotate to angle example

---
+++ ProtoBot/RotateToAngle-Imported/src/main/include/Robot.h
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/drive/MecanumDrive.h>
+#include <frc/Joystick.h>
+// #include <frc/Spark.h>
+#include <frc/DriverStation.h>
+#include <frc/PIDOutput.h>
+#include <frc/PIDController.h>
+#include "AHRS.h"
+#include <frc/SpeedControllerGroup.h>
+#include "ctre/Phoenix.h"
+#include <frc/drive/DifferentialDrive.h>
+
+using namespace frc;
+
+class Robot : public TimedRobot, public PIDOutput {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+
+  void PIDWrite(double output) override;
+
+ private:
+    // Channels for the wheels
+    const static int frontLeftChannel = 2;
+    const static int rearLeftChannel = 3;
+    const static int frontRightChannel = 1;
+    const static int rearRightChannel = 0;
+
+    WPI_TalonSRX m_leftfront{1};
+    WPI_TalonSRX m_leftrear{2};
+    WPI_TalonSRX m_rightfront{7};
+    WPI_TalonSRX m_rightrear{8};
+    WPI_TalonSRX m_shooter{6};
+    frc::SpeedControllerGroup m_left{m_leftfront, m_leftrear};
+    frc::SpeedControllerGroup m_right{m_rightfront, m_rightrear};
+    frc::DifferentialDrive m_robotDrive{m_left, m_right};
+
+    const static int joystickChannel = 0;
+
+    MecanumDrive *robotDrive; // robot drive system
+    Joystick *stick;          // only joystick
+    AHRS *ahrs;
+    PIDController *turnController;      // PID Controller
+    double rotateToAngleRate;           // Current rotation rate
+};
